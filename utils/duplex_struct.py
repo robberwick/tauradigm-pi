d3e24e0a7a103ca3389a0869daeee1fb542a4aad
Rob Berwick <rob.berwick@gmail.com>
2020-01-11 22:00:55 +0000

    update duplex_struct for 8 float test

--- utils/duplex_struct.py
+++ utils/duplex_struct.py
@@ -9,19 +9,19 @@ if __name__ == '__main__':
     try:
         link = txfer.SerialTransfer('/dev/serial0', baud=1152000, restrict_ports=False)
         while True:
-            x = random.uniform(-500, 500)
-            y = random.uniform(-500, 500)
-            z = random.uniform(-500, 500)
-            payload = struct.pack('fff', x, y, z)
-            for i, b in enumerate(list(payload)):
-                link.txBuff[i] = b
-            link.send(len(payload))
+            # x = random.uniform(-500, 500)
+            # y = random.uniform(-500, 500)
+            # z = random.uniform(-500, 500)
+            # payload = struct.pack('fff', x, y, z)
+            # for i, b in enumerate(list(payload)):
+            #     link.txBuff[i] = b
+            # link.send(len(payload))
 
             if not link.available():
                 if link.status < 0:
                     print('ERROR: {}'.format(link.status))
             else:
-                payload = struct.unpack('bbbbbbbbbbbbBBBBBBBB', bytearray(link.rxBuff[0:link.bytesRead]))
+                payload = struct.unpack('ffffffff', bytearray(link.rxBuff[0:link.bytesRead]))
                 print(payload)
             time.sleep(0.02)
 
