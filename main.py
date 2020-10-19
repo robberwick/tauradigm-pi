376f76f09a48a75b136965f247ba36a269b17179
Rob Berwick <rob.berwick@gmail.com>
2020-10-19 17:25:25 +0100

    minesweeper code purged, working

--- main.py
+++ main.py
@@ -101,26 +101,6 @@ def run():
                         if 'home' in joystick.presses:
                             logger.info('Home button pressed - exiting')
                             raise RobotStopException()
-                        if joystick.circle:
-                            kp = 0.05
-                            if log_data is not None:
-                                target_distance = 540
-                                distance_sensor1 = min(2000, log_data[3])
-                                steering_compensation = int((distance_sensor1 - target_distance)* kp)
-                                lit_threshold = 750
-                                # light levels are at log_data[21] through to [24] inclusive
-                                # currently  #24 is not working
-                                # stop if max light level is above threshold
-                                stop = max(log_data[21:24]) >= lit_threshold
-
-                            else:
-                                stop = False
-                                steering_compensation = 0
-                            power_left = 26 + steering_compensation
-                            power_right = 20 - steering_compensation
-                            if stop:
-                                power_left = 0
-                                power_right = 0
                         send_motor_speed_message(link=link, left=power_left, right=power_right)
                         if link.available():
                             log_data = (time.time(),)+ receive_sensor_data(link=link)
