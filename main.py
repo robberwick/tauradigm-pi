b89ccc63d3ee762d55ed3e5975e14abf0d9c400b
Rob Berwick <rob.berwick@gmail.com>
2020-11-28 21:41:04 +0000

    left joystick Y position mapped to vortex motor speed and outputted

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ def mixer(yaw, throttle, max_power=100):
     scale = float(max_power) / max(1, abs(left), abs(right))
     return int(left * -scale), int(right * -scale)
 
-def send_motor_speed_message(link=None, left=0, right=0):
-    payload = struct.pack('=bff', 1, right, left)
+def send_motor_speed_message(link=None, left=0, right=0, vortex=0):
+    payload = struct.pack('=bfff', 1, right, left, vortex)
     for i, b in enumerate(list(payload)):
         link.txBuff[i] = b
     # print('sending: {}'.format(payload))
@@ -90,6 +90,11 @@ def run():
                         # Get virtual right axis joystick values from the right analogue stick
                         virt_x_axis, virt_y_axis = joystick['r']
                         power_left, power_right = mixer(yaw=virt_x_axis, throttle=virt_y_axis)
+
+                        # get values for joystick left axis
+                        left_x_axis, left_y_axis = joystick['l']
+                        downforce = 100 * left_y_axis
+
                         # Get a ButtonPresses object containing everything that was pressed since the last
                         # time around this loop.
                         joystick.check_presses()
@@ -121,7 +126,7 @@ def run():
                             if stop:
                                 power_left = 0
                                 power_right = 0
-                        send_motor_speed_message(link=link, left=power_left, right=power_right)
+                        send_motor_speed_message(link=link, left=power_left, right=power_right, vortex=downforce)
                         if link.available():
                             log_data = (time.time(),)+ receive_sensor_data(link=link)
                             logger.log('DATA', ','.join(map(str,log_data)))
