219a7a11e884c3f7e6fe9b3663f8af9ec2346b07
markmellors <mark.mellors@gmail.com>
2021-06-07 21:04:32 +0100

    state map fleshed out, first untested draft

--- turn_signal_states.py
+++ turn_signal_states.py
@@ -1,17 +1,61 @@
+
+import time
 from transitions import Machine
+from transitions.extensions.states import add_state_features, Timeout
 
-class CaptureSequence(aelf):
+@add_state_features(Tags, Timeout)
+class CaptureSequence(self):
 
-    states = ['waiting for signal', 'waiting for delimiter', 'outputting', 'timed out']
+    states = [{'name': 'wait sequence start'},
+              {'name': 'wait signal', 'timeout': 5, 'on_timeout': 'timed_out'}
+              ('name': 'sequence complete'}]
 
-    self.current_turn_signal = 0
-    self.number_of_turns = 3
+    def __init__(self):
+        self.signal_timeout = 5
+        self.last_signal_time = 0
 
-    #timeout is seconds from initia
-    self.signal_timeout = 5
+        self.current_signal_number = 0
+        self.number_of_turns = 3
+        self.auto_transitions=False
+        self.waiting_for_signal = False
+        self.turn_sequence = [self.number_of_turns]
 
-    self.machine = Machine(model=self, states=CaptureSequence.states, initial='waiting for signal')
+        self.signal_key = {
+            '1*': 'delimiter',
+            '2*': 'left',
+            '3*': 'right'
+        }
+        self.machine = Machine(model=self, states=CaptureSequence.states, initial='wait sequence start')
 
+        self.machine.add_transtion('start', 'wait sequence start', 'wait signal')
+        self.machine.add_transtion('signal_received', 'wait signal', '=', 'before':['reset_timeout', 'process_signal'])
+        self.machine.add_transtion('complete_sequence_received', 'wait signal', 'sequence complete')
+        self.machine.add_transtion('reset', 'sequence complete', 'wait sequence start')
+        self.machine.add_transtion('timed_out', '*', 'wait sequence start')
 
    ##commands
-  
+
+    def reset_timeout(self, signal): self.last_signal_time = time.clock()
+
+    def proces_signal(self, note):
+        signal =  self.signal_key[note]
+        if self.waiting_for_signal
+            if signal == 'delimiter':
+                #waiting for a signal but still getting delimiter, carry on waiting
+                break
+            else:
+                #waiting for signal and got a signal, process it and move on
+                self.turn_sequence[self.current_signal_number] = signal
+                self.current_signal_number += 1
+                self.waiting_for_signal = False
+                if self.current_signal_number >= self.number_of_turns:
+                    #we got all the turns we were expecting
+                    self.complete_sequence_received()
+        else:
+                #waiting for delimiter and got it, so next we're looking for a signal
+                self.waiting_for_signal = True
+            else:
+                #we got another signal whilst waiting for a delimiter, ignore it
+                break
+
+
