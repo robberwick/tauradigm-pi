4bc46889f470bb48c3ffe39170da6250753fd98d
markmellors <mark.mellors@gmail.com>
2021-06-08 21:15:45 +0100

    simplified to only essential elements

--- frequency_detection.py
+++ frequency_detection.py
@@ -1,118 +1,41 @@
 #!/usr/bin/env python3
-"""Show a text-mode spectrogram using live microphone data."""
-import argparse
 import math
-import shutil
-
+from time import sleep
 import numpy as np
 import sounddevice as sd
 
-usage_line = ' press <enter> to quit, +<enter> or -<enter> to change scaling '
-
-def int_or_str(text):
-    """Helper function for argument parsing."""
-    try:
-        return int(text)
-    except ValueError:
-        return text
-
 
 try:
-    columns, _ = shutil.get_terminal_size()
-except AttributeError:
-    columns = 100
-
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__ + '\n\nSupported keys:' + usage_line,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    '-b', '--block-duration', type=float, metavar='DURATION', default=50,
-    help='block size (default %(default)s milliseconds)')
-parser.add_argument(
-    '-c', '--columns', type=int, default=columns,
-    help='width of spectrogram')
-parser.add_argument(
-    '-d', '--device', type=int_or_str,
-    help='input device (numeric ID or substring)')
-parser.add_argument(
-    '-g', '--gain', type=float, default=10,
-    help='initial gain factor (default %(default)s)')
-parser.add_argument(
-    '-r', '--range', type=float, nargs=2,
-    metavar=('LOW', 'HIGH'), default=[320, 1200],
-    help='frequency range (default %(default)s Hz)')
-args = parser.parse_args(remaining)
-low, high = args.range
-if high <= low:
-    parser.error('HIGH must be greater than LOW')
-
-# Create a nice output gradient using ANSI escape sequences.
-# Stolen from https://gist.github.com/maurisvh/df919538bcef391bc89f
-colors = 30, 34, 35, 91, 93, 97
-chars = ' :%#\t#%:'
-gradient = []
-for bg, fg in zip(colors, colors[1:]):
-    for char in chars:
-        if char == '\t':
-            bg, fg = fg, bg
-        else:
-            gradient.append('\x1b[{};{}m{}'.format(fg, bg + 10, char))
-
-try:
-    samplerate = sd.query_devices(args.device, 'input')['default_samplerate']
-
-    delta_f = (high - low) / (args.columns - 1)
+    samplerate = sd.query_devices(kind='input')['default_samplerate']
+    bins = 156
+    low = 320            #hz
+    high = 1200          #hz
+    gain = 10
+    delta_f = (high - low) / (bins)
     fftsize = math.ceil(samplerate / delta_f)
     low_bin = math.floor(low / delta_f)
-
+    block_duration = 50  #milliseconds
 
     def callback(indata, frames, time, status):
-        if status:
-            text = ' ' + str(status) + ' '
-            print('\x1b[34;40m', text.center(args.columns, '#'),'\x1b[0m', sep=' ')
         if any(indata):
             magnitude = np.abs(np.fft.rfft(indata[:, 0], n=fftsize))
-            magnitude *= args.gain / fftsize
+            magnitude *= gain / fftsize
             max_component = np.amax(magnitude)
             dominant_frequency = np.where(magnitude == np.amax(magnitude))[0][0]
             if (max_component > 0.1):
                 df = dominant_frequency
-                print("1*") if (100 < df < 120) else ""
-                print("2*") if (120 < df < 143) else ""
-                print("3*") if (143 < df < 160) else ""
-                print("4*") if (160 < df < 179) else ""
-                print("5*") if (179 < df < 200) else ""
-#            line = (gradient[int(np.clip(x, 0, 1) * (len(gradient) - 1))]
-#                    for x in magnitude[low_bin:low_bin + args.columns])
-#            print(*line, sep=' ', end='\x1b[0m\n')
+                if (100 < df < 120): print("1*")
+                if (120 < df < 143): print("2*")
+                if (143 < df < 160): print("3*")
+                if (160 < df < 179): print("4*")
+                if (179 < df < 200): print("5*")
         else:
             print('no input')
 
-    with sd.InputStream(device=args.device, channels=1, callback=callback,
-                        blocksize=int(samplerate * args.block_duration / 1000),
+    with sd.InputStream(device=None, channels=1, callback=callback,
+                        blocksize=int(samplerate * block_duration / 1000),
                         samplerate=samplerate):
         while True:
             response = input()
-            if response in ('', 'q', 'Q'):
-                break
-            for ch in response:
-                if ch == '+':
-                    args.gain *= 2
-                elif ch == '-':
-                    args.gain /= 2
-                else:
-                    print('\x1b[31;40m', usage_line.center(args.columns, '#'),'\x1b[0m', sep=' ')
-                    break
 except KeyboardInterrupt:
-    parser.exit('Interrupted by user')
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
+    print("exiting")
