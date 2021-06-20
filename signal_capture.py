1e3a4d29b16bba6793f8a04917b09cd7ef144e48
Mark Mellors <Mark.Mellors@gmail.com>
2021-06-20 19:15:09 +0100

    converted signal_capture into a class

--- signal_capture.py
+++ signal_capture.py
@@ -5,25 +5,31 @@ import numpy as np
 import sounddevice as sd
 from turn_signal_states import CaptureSequence
 
-try:
-    #audio  capture stuff:
-    samplerate = sd.query_devices(kind='input')['default_samplerate']
-    bins = 156
-    low = 320            #hz
-    high = 1200          #hz
-    gain = 10
-    delta_f = (high - low) / (bins)
-    fftsize = math.ceil(samplerate / delta_f)
-    low_bin = math.floor(low / delta_f)
-    block_duration = 50  #milliseconds
+class AudioCapture():
+    def __init__(self):
+        self.last_max_component = 0
+        self.gain = 10
+        self.running = True
+        bins = 156
+        low = 320            #hz
+        high = 1200          #hz
+        delta_f = (high - low) / (bins)
+        low_bin = math.floor(low / delta_f)
+        self.block_duration = 50  #milliseconds
+        try:
+            self.samplerate = sd.query_devices(kind='input')['default_samplerate']
+            self.fftsize = math.ceil(self.samplerate / delta_f)
+        except KeyboardInterrupt:
+            print("exiting")
+            self.running = False
 
-    def callback(indata, frames, time, status):
+    def callback(self, indata, frames, time, status):
         if any(indata):
-            magnitude = np.abs(np.fft.rfft(indata[:, 0], n=fftsize))
-            magnitude *= gain / fftsize
+            magnitude = np.abs(np.fft.rfft(indata[:, 0], n=self.fftsize))
+            magnitude *= self.gain / self.fftsize
             max_component = np.amax(magnitude)
             dominant_frequency = np.where(magnitude == np.amax(magnitude))[0][0]
-            if (max_component > 0.12):
+            if (max_component > 0.12) and (max_component > self.last_max_component):
                 df = dominant_frequency
                 signal = None
                 if (100 < df < 120): signal = "1*"
@@ -36,21 +42,20 @@ try:
                     sequence.signal_received(signal)
                 except:
                     pass
+            self.last_max_component = max_component
         else:
             print('no input')
 
-    #start sequence FSM
-    sequence = CaptureSequence()
-    sequence.start()
+    def get_sequence(self):
+            #start sequence FSM
+            sequence = CaptureSequence()
+            sequence.start()
 
-    with sd.InputStream(device=None, channels=1, callback=callback,
-                        blocksize=int(samplerate * block_duration / 1000),
-                        samplerate=samplerate):
-        while True:
-#            response = input()
-            sleep(0.001)
-            if sequence.state == 'sequence complete':
-                break
+            with sd.InputStream(device=None, channels=1, callback=self.callback,
+                                blocksize=int(self.samplerate * self.block_duration / 1000),
+                                samplerate=self.samplerate):
+                while self.running:
+                    sleep(0.001)
+                    if sequence.state == 'sequence complete':
+                        return sequence.turn_sequence
 
-except KeyboardInterrupt:
-    print("exiting")
