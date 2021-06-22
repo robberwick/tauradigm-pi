eaf89f065f7af6fac5f4d2c2263c36ace1fbaf6d
Mark Mellors <Mark.Mellors@gmail.com>
2021-06-22 23:55:44 +0100

    basic state transitions written and working

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from loguru import logger
 from pySerialTransfer import pySerialTransfer as txfer
 import navigation
 from navigation import Pose
+from enum import IntEnum
 
 data_filter = lambda record: record["level"].name == "DATA"
 non_data_filter = lambda record: record["level"].name != "DATA"
@@ -26,6 +27,22 @@ logger.add(sys.stdout, level="INFO", format=fmt, filter=non_data_filter)
 logger.add("logs/info_1.log", level="INFO", format=fmt, rotation="5 MB", filter=non_data_filter)
 logger.add("logs/data_{time}.log", level="DATA", format="{message}", filter=data_filter)
 log_data = None
+
+class Mode(IntEnum):
+    travelling = 0,
+    travelled = 1,
+    rotating = 2,
+    rotated = 3,
+    firing = 4,
+    fired = 5,
+    reloading = 6,
+    reloaded = 7
+
+start_position = Pose(0, 0, 0)
+midway = Pose(450, 0, 0)
+start_heading = Pose(0, 0, 0)
+firing_heading = Pose(0, 0, -1.47)
+firing_position = Pose(450, -300, 0)
 
 class RobotStopException(Exception):
     pass
@@ -89,7 +106,7 @@ def send_rotate_message(link=None, pose=None):
 
 def unpack_log_message(link=None):
 #    fmt = 'f' * 8 + 'l' * 6 + 'f' * 3 + 'f' * 3
-    fmt = 'f' * 3 + 'f' * 3
+    fmt = 'f' * 3 + 'f' * 3 + 'b'
 
     response = array.array('B', link.rxBuff[:link.bytesRead]).tobytes()
 
@@ -97,15 +114,77 @@ def unpack_log_message(link=None):
 
 def extract_current_pose(log_vars):
     """Extract the current pose from the tuple of values extracted from the log message"""
-    *_, x, y, heading = log_vars
+    *_, x, y, heading, current_mode = log_vars
     return navigation.Pose(x, y, heading)
 
+def extract_current_mode(log_vars):
+    """Extract the current mode from the tuple of values extracted from the log message"""
+    *_, current_mode = log_vars
+    return Mode(current_mode)
+
+def feed_the_fish(link, current_mode, process_step):
+    new_process_step = process_step
+    if process_step == 0 and current_mode != Mode.travelling:
+        send_straight_move_message(link=link, pose=midway)
+        print("moving to midway")
+    if process_step == 0 and current_mode == Mode.travelling:
+        print("moving to midway")
+        new_process_step = 1
+    elif process_step == 1 and current_mode == Mode.travelled:
+        send_rotate_message(link=link, pose=firing_heading)
+        print("sending rotating command")
+    elif process_step == 1 and current_mode == Mode.rotating:
+        print("rotating")
+        new_process_step = 2
+    elif process_step == 2 and current_mode == Mode.rotated:
+        send_straight_move_message(link=link, pose=firing_position)
+        print("sending move to firing")
+    elif process_step == 2 and current_mode == Mode.travelling:
+        new_process_step = 3
+    elif process_step == 3 and current_mode == Mode.travelled:
+        send_rotate_message(link=link, pose=firing_heading)
+        print("sending aiming command")
+    elif process_step == 3 and current_mode == Mode.rotating:
+        print("rotating")
+        new_process_step = 4
+    elif process_step == 4 and current_mode == Mode.rotated:
+        send_button_press_message(link,button=b't')
+        print("moved to firing")
+    elif process_step == 4 and current_mode == Mode.fired:
+        send_straight_move_message(link=link, pose=midway)
+        print("fired")
+    elif process_step == 4 and current_mode == Mode.travelling:
+        print("moving back to midway")
+        new_process_step = 5
+    elif process_step == 5 and current_mode == Mode.travelled:
+        send_rotate_message(link=link, pose=start_heading)
+        print("moved back to midway")
+    elif process_step == 5 and current_mode == Mode.rotating:
+        print("rotating back to start")
+        new_process_step = 6
+    elif process_step == 6 and current_mode == Mode.rotated:
+        send_straight_move_message(link=link, pose=start_position)
+        print("rotated back for start")
+    elif process_step == 6 and current_mode == Mode.travelling:
+        send_straight_move_message(link=link, pose=start_position)
+        print("moving back to start")
+        new_process_step = 7
+    elif process_step == 7 and current_mode == Mode.travelled:
+        print("moved back to start")
+        new_process_step = 8
+    return new_process_step
+    
 @click.command()
 @click.option('--waypoints', default=None, help='waypoint file')
 def run(waypoints=None):
     waypoint_list = None
     navigating = False
     driving = True
+    fish_feeding = False
+    current_mode = None
+    process_step = 0
+    shot_number = 1
+    shots_allowed = 3
 
     try:
         link = txfer.SerialTransfer('/dev/serial0', baud=500000, restrict_ports=False)
@@ -149,13 +228,16 @@ def run(waypoints=None):
                         if joystick.has_presses:
                             time.sleep(0.06)
                             logger.debug(joystick.presses)
+                            if joystick.presses.start:
+                                print("started")
+                                fish_feeding = True
                             if joystick.presses.circle:
                                 #send_button_press_message(link,button=b'c')
                                 logger.info('circle button pressed')
                                 test_B = Pose(450, 0, 0)
                                 send_straight_move_message(link=link, pose=test_B)
                             if joystick.presses.triangle:
-                                #send_button_press_message(link,button=b't')
+                                send_button_press_message(link,button=b't')
                                 logger.info('triangle button pressed')
                                 driving = True
                             if joystick.presses.square:
@@ -167,6 +249,7 @@ def run(waypoints=None):
                                 send_button_press_message(link,button=b'x')
                                 logger.info('cross button pressed')
                                 driving = False
+                                fish_feeding = False
                             if joystick.presses.dleft:
                                 send_button_press_message(link,button=b'l')
                                 logger.info('D pad left pressed')
@@ -201,8 +284,8 @@ def run(waypoints=None):
                             raise RobotStopException()
                         if not driving:
                             power_left, power_right = 0, 0
-                        if not navigating:
-                            send_motor_speed_message(link=link, left=power_left, right=power_right)
+                        send_motor_speed_message(link=link, left=power_left, right=power_right)
+                        time.sleep(0.03)
                         if link.available():
                             # unpack the incoming log message into a tuple
                             message_values = unpack_log_message(link=link)
@@ -211,9 +294,21 @@ def run(waypoints=None):
                             log_data = (time.time(),) + message_values
                             logger.log('DATA', ','.join(map(str,log_data)))
                             # Get the current pose from the message values
-                            current_pose = extract_current_pose(message_values)
+                            current_mode = extract_current_mode(message_values)
                             # Is the current position close enough to the target waypoint to select the next one?
 
+                            if fish_feeding:
+                                if process_step == 8:
+                                    #we've travelled back to the square, so set mode back to start
+                                    # and get ready to go again
+                                    process_step = 0
+                                    if shot_number < shots_allowed:
+                                        shot_number += 1
+                                        process_step = feed_the_fish(link, current_mode, process_step)
+                                    else:
+                                        fish_feeding = False
+                                else:
+                                    process_step = feed_the_fish(link, current_mode, process_step)
                         else:
                             link_msg = 'no data - link status: {}'.format(link.status)
                             logger.info(link_msg)
